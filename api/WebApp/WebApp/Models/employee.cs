77bae4d267d3129495a5aed4f0c58e8c161655eb
mmdrza0938 <mahareza1999@gmail.com>
2022-09-28 16:28:00 +0330

    user-api

---
+++ api/WebApp/WebApp/Models/employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int employeeId { get; set; }
+        public string employeeName { get; set; }
+        public string department { get; set; }
+
+        public string dateOfJoining { get; set; }
+
+        public string PhotoFileName { get; set; }
+    }
+}
